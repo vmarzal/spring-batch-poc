a9f40dd6cdc7fac851e28e8d52e3095a42e81339
Victor <victor.marzal@sistemasgenomicos.com>
2019-09-05 21:54:07 +0200

    first commit

---
+++ src/main/java/com/mongodb/batch/genome/GenomeApplication.java
@@ -0,0 +1,13 @@
+package com.mongodb.batch.genome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenomeApplication.class, args);
+	}
+
+}
